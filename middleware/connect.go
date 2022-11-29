cbdb01acd373290d6048680df999751006ad125c
boslagu <m.rommellagurin@gmail.com>
2022-11-29 15:23:36 +0800

    Added encryption function.

--- middleware/connect.go
+++ middleware/connect.go
@@ -3,20 +3,41 @@ package middleware
 
 import (
 	"Template/middleware/go-utils/database"
+	"Template/middleware/go-utils/encryptDecrypt"
 	httpUtils "Template/middleware/go-utils/http"
+	"fmt"
 
 	"log"
 	"net/http"
 )
 
 func CreateConnection() {
-
+	username, dbConfigErr := encryptDecrypt.Decrypt(GetEnv("POSTGRES_USERNAME"), GetEnv("SECRET_KEY"))
+	if dbConfigErr != nil {
+		fmt.Println("error encrypting your classified text: ", dbConfigErr)
+	}
+	password, dbConfigErr := encryptDecrypt.Decrypt(GetEnv("POSTGRES_PASSWORD"), GetEnv("SECRET_KEY"))
+	if dbConfigErr != nil {
+		fmt.Println("error encrypting your classified text: ", dbConfigErr)
+	}
+	host, dbConfigErr := encryptDecrypt.Decrypt(GetEnv("POSTGRES_HOST"), GetEnv("SECRET_KEY"))
+	if dbConfigErr != nil {
+		fmt.Println("error encrypting your classified text: ", dbConfigErr)
+	}
+	dbName, dbConfigErr := encryptDecrypt.Decrypt(GetEnv("DATABASE_NAME"), GetEnv("SECRET_KEY"))
+	if dbConfigErr != nil {
+		fmt.Println("error encrypting your classified text: ", dbConfigErr)
+	}
+	fmt.Println("username: ", username)
+	fmt.Println("password: ", password)
+	fmt.Println("host: ", host)
+	fmt.Println("dbName: ", dbName)
 	httpUtils.Client.New(&http.Client{})
 	database.PostgreSQLConnect(
-		GetEnv("POSTGRES_USERNAME"),
-		GetEnv("POSTGRES_PASSWORD"),
-		GetEnv("POSTGRES_HOST"),
-		GetEnv("DATABASE_NAME"),
+		username,
+		password,
+		host,
+		dbName,
 		GetEnv("POSTGRES_PORT"),
 		GetEnv("POSTGRES_SSL_MODE"),
 		GetEnv("POSTGRES_TIMEZONE"),
